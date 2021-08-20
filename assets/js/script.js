1587fd2735ed127e2738285f801132844fdf6604
Theo <trgrf1@gmail.com>
2021-08-20 15:01:44 -0400

    saving changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
 userFormEl = $('#user-input');
 
 function presentWeather() {
-    
+
 }
 
 function getApi() {
+    var url = ""
+
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        presentWeather(data);
+    })
 
 }
 
 function FormSubmitHandler() {
+    var userInput = $('city-name');
 
+    if (userInput) {
+        getApi(userInput);
+
+        $('city-name').val = "";
+    }
 }
 
 userFormEl.on('click', FormSubmitHandler);
