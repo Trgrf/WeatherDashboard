1befec89e774af3c9d0ae764a3654c19aa3b385b
Theo <trgrf1@gmail.com>
2021-08-22 16:34:51 -0400

    saving changes to html layout and js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 function presentWeather(stats) {
-for (var i = 0; stats.main.length; i++) {
-    var stasList = $('<ul>') ;
-    var tempEl = $('<li>');
-    tempEl.text(stats.main[i].temp + "° F");
+    console.log(stats);
+    var tempEl = $('<h3>');
+    var cityName = $('<h2>').addClass('card-title').text(stats.name);
+    var icon = "*"
+    tempEl.text("Temp: " + Math.round(stats.main) + "° F");
     // var windEl = ;
     // var humidityEl = ;
 
-    stasList.append(tempEl);
-    $('.container').append(stasList);
+
+    $('#current-weather').append(cityName.append(icon), tempEl);
+    getApi2(stats.coord.lat, stats.coord.lon);
+}
+
+function forecastWeather(stats) {
+    console.log(stats.daily[1]);
+for (var i= 1; i < 6; i++) {
+    var day = moment.unix(stats.daily[i].dt).format('dddd ') ;
+    var col = $('<div>').addClass('col-2')
+    var card = $('<div>').addClass('card').attr('style', 'border: 1px solid black');
+    var cardTitle = $("<h4>").addClass('card-title p-1').text(day);
+
+    $('#forecast-bar').append(col.append(card.append(cardTitle)));
 }
 }
 
@@ -21,6 +34,19 @@ function getApi(city) {
     .then(function(data) {
         console.log(data);
         presentWeather(data);
+    })
+
+}
+
+function getApi2(lat, lon) {
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&exclude=minutely&appid=61fa702c7ad2637a7e4e11d44c6c28a1"
+
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        forecastWeather(data); 
     })
 
 }
